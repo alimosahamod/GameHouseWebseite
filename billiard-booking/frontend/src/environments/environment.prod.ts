16401281498c77d94ddafddb734375742ad336f4
Ali <110ya110@gmail.com>
2026-01-03 22:36:07 +0100

    AWS Adjustments updated docker-compose.yml added Frontend Dockerfile added apiURL in environment.prod.ts

--- billiard-booking/frontend/src/environments/environment.prod.ts
+++ billiard-booking/frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // WICHTIG: Ersetze DEINE-AWS-IP mit der echten Public IP deiner EC2-Instanz
-  apiUrl: 'http://DEINE-AWS-IP:3000'
+  apiUrl: 'http://16.16.123.141:3000'
 };

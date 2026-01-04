73d6049288d7e0fbfc604b6b0b2352d143c1e138
Ali <110ya110@gmail.com>
2026-01-04 01:01:47 +0100

    AWS Adjustments updated environment.prod.ts updated environment.ts updated package.json

--- billiard-booking/frontend/src/environments/environment.prod.ts
+++ billiard-booking/frontend/src/environments/environment.prod.ts
@@ -2,4 +2,5 @@ export const environment = {
   production: true,
   // WICHTIG: Ersetze DEINE-AWS-IP mit der echten Public IP deiner EC2-Instanz
   apiUrl: 'http://16.16.123.141:3000'
+  //apiUrl: 'http://localhost:3000'
 };
--- billiard-booking/frontend/src/environments/environment.ts
+++ billiard-booking/frontend/src/environments/environment.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000' // Dein lokales Backend
+  //apiUrl: 'http://localhost:3000' // Dein lokales Backend
+  apiUrl: 'http://16.16.123.141:3000'
 };
+
+
